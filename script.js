7490cc066ea589c465dd05f7bf752da836c5afcd
arthurpedroni <arthur.pedroni2@gmail.com>
2024-05-09 14:57:54 -0300

    l

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function previewImage(event) {
-    var reader = new FileReader();
-    reader.onload = function() {
-        var output = document.getElementById('imagePreview');
-        output.style.backgroundImage = 'url(' + reader.result + ')';
+    var input = event.target;
+    var preview = document.getElementById('imagemPreview');
+    preview.innerHTML = '';
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function(e) {
+        var img = document.createElement("img");
+        img.src = e.target.result;
+        preview.appendChild(img);
+      }
+      reader.readAsDataURL(input.files[0]);
     }
-    reader.readAsDataURL(event.target.files[0]);
-}
+  }
