37a0b6cad0d62cf3eccfb1c01f0061c03249d612
arthurpedroni <arthur.pedroni2@gmail.com>
2024-05-12 21:36:06 -0300

    d

--- entrar.js
+++ entrar.js
@@ -1,8 +1,8 @@
 function entrar(){
-    var senha = document.getElementById("senha").value;
-    var email = document.getElementById("email").value;
-    var email_local = localStorage. getItem("email");
-    var senha_local = localStorage. getItem("senha");
+    var senha = document. getElementById ("senha").value;
+    var email = document. getElementById ("email").value;
+    var email_local = localStorage. getItem ("email");
+    var senha_local = localStorage. getItem ("senha");
 
     if(senha === senha_local && email === email_local){
         alert("Seja bem vindo a StartupInvest!")
